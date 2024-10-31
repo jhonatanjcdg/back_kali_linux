f863a7a1a0d6937de4984a36f8aab0f47d0f143b
jhonatanjcdg <j3172224877@gmail.com>
2024-10-31 16:59:58 -0500

    Funciona nodemailer

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { config } = require ('dotenv');
+const { EMAIL_FROM, EMAIL_PASS } = require("./.env");
+const morgan = require('morgan');
+config({ path: '.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(morgan("dev"))
 app.use(bodyParser.json());
 
 // Configurar nodemailer
 const transporter = nodemailer.createTransport({
   service: 'gmail', // Usa 'gmail' o tu servicio de correo
   auth: {
-    user: 'tu-email@gmail.com',
-    pass: 'tu-contraseña' // O usa una contraseña de aplicación
+    user: process.env.EMAIL_FROM,
+    pass: process.env.EMAIL_PASS // O usa una contraseña de aplicación
   }
 });
 
@@ -25,7 +30,7 @@ app.post('/send-email', (req, res) => {
 
   const mailOptions = {
     from: email,
-    to: 'destino@example.com', // Reemplaza con tu email de destino
+    to: process.env.EMAIL_FROM, // Reemplaza con tu email de destino
     subject: `Nuevo mensaje de ${name}`,
     text: message,
   };
